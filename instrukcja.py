ae9337019ee75ceab874e17c253b67d11c2df90e
Paulina <paulina.szczepanska122@gmail.com>
2018-10-28 14:28:02 +0100

    Initial commit

---
+++ instrukcja.py
@@ -0,0 +1,27 @@
+def instructions():
+    """Wyświetl instrukcję gry"""
+    print(
+    """
+    Witaj w największym intelektualnym wyzwaniu wszech czasów, jakim jest
+    gra 'Kółko i krzyżyk'. Będzie to ostateczna rozgrywka między Twoim
+    ludzkim mózgiem a moim krzemowym procesorem.
+    
+    Swoje posunięcie wskażesz poprzez wprowadzenie liczby z zakresu 0 - 8.
+    Liczba ta odpowiada pozycji na planszy zgodnie z poniższym schematem:
+    
+                    0 | 1 | 2
+                    --+---+--
+                    3 | 4 | 5
+                    --+---+--
+                    6 | 7 | 8
+                    
+    Przygotuj się, Człowieku. Ostateczna batalia niebawem się rozpocznie. \n
+    """
+    )
+
+
+print("Oto instrukcja do gry 'Kółko i krzyżyk':")
+instructions()
+print("Ponownie ta sama instrukcja:")
+instructions()
+print("Prawdopodobnie teraz już zrozumiałeś tą grę.")
